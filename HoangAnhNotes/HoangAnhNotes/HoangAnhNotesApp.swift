6cb18ef22238f6059eb6ca3092aaabf4d8164ad7
Hoang Anh <truonghoanganh.231220@gmail.com>
2023-11-22 20:13:35 +0700

    new project

---
+++ HoangAnhNotes/HoangAnhNotes/HoangAnhNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoangAnhNotesApp.swift
+//  HoangAnhNotes
+//
+//  Created by Hoang Anh on 22/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct HoangAnhNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
